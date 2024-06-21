cea460549949a711b2ffd2b022768ca28c798544
borossandor27 <boros.sanyi@gmail.com>
2024-06-21 13:33:57 +0200

    Udvozles komponenssel

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Alice" />
+      <Welcome name="Bob" />
+    </div>
+  );
+}
+
+export default App;
